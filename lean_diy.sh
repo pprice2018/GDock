cdf88d1061219acdf5d08f1eb8f87ff22ff7de4d
kyarazhan <shangrilas@live.com>
2020-04-10 09:10:21 +0800

    同步上游

---
+++ lean_diy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/Openwrt/Kyara SSR+/g' package/base-files/files/bin/config_generate
+sed -i 's/Openwrt/Kyara SSR+/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+./scripts/feeds update -a
+./scripts/feeds install -a
--- vssr_diy.sh
+++ vssr_diy.sh
@@ -7,10 +7,9 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-sed -i 's/Openwrt/Kyara/g' package/base-files/files/bin/config_generate
-sed -i 's/Openwrt/Kyara/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/Openwrt/Kyara HelloWorld/g' package/base-files/files/bin/config_generate
+sed -i 's/Openwrt/HelloWorld/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
-git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 ./scripts/feeds update -a
 ./scripts/feeds install -a

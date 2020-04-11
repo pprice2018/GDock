ba641924907e893880b12f10d5fb7d36d8c73448
kyarazhan <shangrilas@live.com>
2020-04-11 10:31:51 +0800

    修改WiFi名称

--- vssr_diy.sh
+++ vssr_diy.sh
@@ -8,7 +8,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/Openwrt/Kyara HelloWorld/g' package/base-files/files/bin/config_generate
-sed -i 's/Openwrt/HelloWorld/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/Openwrt/Kyara/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 ./scripts/feeds update -a

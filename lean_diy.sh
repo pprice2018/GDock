25dfb88e26af7473f16e22785d0862f6548b1196
kyarazhan <shangrilas@live.com>
2020-04-16 14:01:34 +0800

    修复大雕删库

--- lean_diy.sh
+++ lean_diy.sh
@@ -9,5 +9,6 @@
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/Openwrt/Kyara SSR+/g' package/base-files/files/bin/config_generate
 sed -i 's/Openwrt/Kyara/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+git clone https://github.com/fw876/helloworld package/SSR_Plus
 ./scripts/feeds update -a
 ./scripts/feeds install -a
